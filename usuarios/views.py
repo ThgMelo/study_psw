229be5535b0056d453afdf3b20e432453e42957d
ThgMelo <es.thg.melo@gmail.com>
2024-01-16 19:51:10 -0300

    feat: login - url - html

--- usuarios/views.py
+++ usuarios/views.py
@@ -9,7 +9,7 @@ def cadastro(request):
     elif request.method == 'POST':
         username = request.POST.get('username')
         senha = request.POST.get('senha')
-        confirma_senha = request.POST.get('confirma_senha')
+        confirma_senha = request.POST.get('confirmar_senha')
 
         if not senha == confirma_senha:
             messages.add_message(request, constants.ERROR, 'Senhas não correspondem')
@@ -30,3 +30,7 @@ def cadastro(request):
         except:
             messages.add_message(request, constants.ERROR, 'Erro interno do servidor')
             return redirect('/usuarios/cadastro')
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
