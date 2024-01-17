a392c9adf70993b9157e3313d7da1b71d55d91be
ThgMelo <es.thg.melo@gmail.com>
2024-01-17 19:37:05 -0300

    feat: enviando dados do formulário

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Categoria
 from .models import Flashcard
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def novo_flashcard(request):
     if not request.user.is_authenticated:
@@ -18,3 +20,32 @@ def novo_flashcard(request):
                 'dificuldades': dificuldades,
             }
         )
+    
+    if request.method == 'POST':
+        pergunta = request.POST.get('pergunta')
+        resposta = request.POST.get('resposta')
+        categoria = request.POST.get('categoria')
+        dificuldade = request.POST.get('dificuldade')
+
+        if len(pergunta.strip()) == 0 or len(resposta.strip()) == 0:
+            messages.add_message(
+                request,
+                constants.ERROR,
+                'Preencha os campos de pergunta e resposta',
+            )
+            return redirect('/flashcard/novo_flashcard')
+        
+        flashcard = Flashcard(
+            user=request.user,
+            pergunta=pergunta,
+            resposta=resposta,
+            categoria_id=categoria,
+            dificuldade=dificuldade,
+        )
+
+        flashcard.save()
+
+        messages.add_message(
+            request, constants.SUCCESS, 'Flashcard cadastrado com sucesso'
+        )
+        return redirect('/flashcard/novo_flashcard')
